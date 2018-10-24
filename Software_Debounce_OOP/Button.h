9c8ea8384f0776473bba185b8159baf051915fe5
Ahid-Naif <ahedqaood19@gmail.com>
2018-10-25 05:32:04 +0800

    add comments

--- Software_Debounce_OOP/Button.h
+++ Software_Debounce_OOP/Button.h
@@ -6,18 +6,21 @@
 class Button
 {
 public:
-  Button(char, int, unsigned long, bool, bool);
+  Button(char, int, unsigned long, bool, bool); // constructor
   void (*Pressed)(void);
   void Refresh();
 
 private:
-  char _portName;
-  int _pin;
+  char _portName; // to store port name
+  int _pin; // to store pin number
   unsigned long _debouncePeriod, _startTimer = 0;
-  bool _isButtonHigh, _isPullUp, _isWaiting = 0, _currentState = 0, _previousState = 0;
+  bool _isButtonHigh; // is button high by default?
+  bool _isPullUp; // is internal Pull up used?
+  bool _isWaiting = 0; // this variable is ON when the button pressed, it is used to wait a bit for the debounce
+  bool _currentState, _previousState; // to store previous & current states of the button
 
-  void initializePorts();
-  void updateStatus();
+  void initialize(); // to set the initial values of the registers & previous state (ON/OFF)
+  void updateStatus(); // to read the state of the button from the register pin B/C/D
 };
 
 #endif
